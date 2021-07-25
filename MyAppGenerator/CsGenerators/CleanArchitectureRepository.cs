df8a7e3ea781e3d33f5f2f2ce2ee8ac8a5d15c4f
bbadawy <bbadawy@minapharm.com>
2021-07-25 15:09:16 +0200

    add SwaggerAuthorized

--- MyAppGenerator/CsGenerators/CleanArchitectureRepository.cs
+++ MyAppGenerator/CsGenerators/CleanArchitectureRepository.cs
@@ -4037,12 +4037,12 @@ namespace " + ApplicationNameSpace + @".Services.Implementations
                         streamWriter.WriteLine(@"
  if (exitRecord != null)
                     {
-            if (exitRecord.Code.ToUpper() == " + UtilityHelper.FormatCamel(className) + "UpsertDto."+ @"Code.ToUpper())
+            if (exitRecord.Code.ToUpper() == " + UtilityHelper.FormatCamel(className) + "UpsertDto." + @"Code.ToUpper())
                         {
                             return new Response<Guid>(string.Format(SD.ExistData, " + UtilityHelper.FormatCamel(className) + "UpsertDto." + @"Code));
                         }
 
- if (exitRecord."+ UtilityHelper.GetColumnNameTitleForCheckExistInDataBase(table)+ ".ToUpper() == "+UtilityHelper.FormatCamel(className) + @"UpsertDto." +
+ if (exitRecord." + UtilityHelper.GetColumnNameTitleForCheckExistInDataBase(table) + ".ToUpper() == " + UtilityHelper.FormatCamel(className) + @"UpsertDto." +
                         UtilityHelper.GetColumnNameTitleForCheckExistInDataBase(table) + @".ToUpper())
                         {
                                 return new Response<" + keyType + @">(string.Format(SD.ExistData, " +
@@ -4051,7 +4051,7 @@ namespace " + ApplicationNameSpace + @".Services.Implementations
 }
     }                       
 ");
-                       
+
                     }
 
                     streamWriter.WriteLine(className + @" " + UtilityHelper.FormatCamel(className) +
@@ -4472,6 +4472,118 @@ namespace " + ApiNameSpace + @".Middleware
 
             #endregion
 
+            #region SwaggerBasicAuthMiddleware Class
+            using (StreamWriter streamWriter =
+                new StreamWriter(Path.Combine(ApiMiddlewarePath, "SwaggerBasicAuthMiddleware.cs")))
+            {
+                // Create the header for the class
+                streamWriter.WriteLine(@"
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace " + ApiNameSpace + @".Middleware
+{
+
+ public class SwaggerBasicAuthMiddleware
+    {
+
+        private readonly RequestDelegate next;
+
+        public SwaggerBasicAuthMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Make sure we are hitting the swagger path, and not doing it locally as it just gets annoying :-)
+            //if (context.Request.Path.StartsWithSegments(""/swagger"") && !this.IsLocalRequest(context))
+            if (context.Request.Path.StartsWithSegments(""/swagger""))
+            {
+                string authHeader = context.Request.Headers[""Authorization""];
+                if (authHeader != null && authHeader.StartsWith(""Basic ""))
+                {
+                    // Get the encoded username and password
+                    var encodedUsernamePassword =
+                        authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+
+                    // Decode from Base64 to string
+                    var decodedUsernamePassword =
+                        Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+                    // Split username and password
+                    var username = decodedUsernamePassword.Split(':', 2)[0];
+                    var password = decodedUsernamePassword.Split(':', 2)[1];
+
+                    // Check if login is correct
+                    if (IsAuthorized(username, password))
+                    {
+                        await next.Invoke(context);
+                        return;
+                    }
+                }
+
+                // Return authentication type (causes browser to show login dialog)
+                context.Response.Headers[""WWW-Authenticate""] = ""Basic"";
+
+                // Return unauthorized
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+
+        public bool IsAuthorized(string username, string password)
+        {
+            // Check that username and password are correct
+            return username.Equals(""MinapharmUser"", StringComparison.InvariantCultureIgnoreCase)
+                   && password.Equals(""Min@pharmP@$$word2020"");
+        }
+
+        public bool IsLocalRequest(HttpContext context)
+        {
+            //Handle running using the Microsoft.AspNetCore.TestHost and the site being run entirely locally in memory without an actual TCP/IP connection
+            if (context.Connection.RemoteIpAddress == null && context.Connection.LocalIpAddress == null)
+            {
+                return true;
+            }
+
+            if (context.Connection.RemoteIpAddress.Equals(context.Connection.LocalIpAddress))
+            {
+                return true;
+            }
+
+            if (IPAddress.IsLoopback(context.Connection.RemoteIpAddress))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static class SwaggerAuthorizeExtensions
+    {
+        public static IApplicationBuilder UseSwaggerAuthorized(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SwaggerBasicAuthMiddleware>();
+        }
+    }  
+
+}
+                                        ");
+
+
+            }
+
+            #endregion
+
             #region BaseApiController Class
             using (StreamWriter streamWriter =
                 new StreamWriter(Path.Combine(ApiInfrastructurePath, "BaseApiController.cs")))
@@ -4795,9 +4907,12 @@ namespace " + ApiNameSpace + @"
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
+        }
+
+             app.UseSwaggerAuthorized();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint(""/swagger/v1/swagger.json"", """ + ApiNameSpace + @" v1""));
-        }
+       
 
 
         dbInitializer.Initialize();
