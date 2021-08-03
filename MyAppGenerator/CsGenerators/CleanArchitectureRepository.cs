66d6ca6d803cba714b31a668fccba7a5bf460f78
bbadawy <bbadawy@minapharm.com>
2021-08-03 15:20:37 +0200

    Inmeory

--- MyAppGenerator/CsGenerators/CleanArchitectureRepository.cs
+++ MyAppGenerator/CsGenerators/CleanArchitectureRepository.cs
@@ -1398,19 +1398,19 @@ namespace " + DataAccessNameSpace + @".Common
 
         public T GetFromSession<T>(string key)
         {
-            var value = _httpContextAccessor?.HttpContext?.Session.GetString(key);
+            var value = _httpContextAccessor?.HttpContext?.Session?.GetString(key);
 
             return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
         public void RemoveFromSession(string key)
         {
-            _httpContextAccessor?.HttpContext?.Session.Remove(key);
+            _httpContextAccessor?.HttpContext?.Session?.Remove(key);
         }
 
         public void SetInSession<T>(string key, T value)
         {
-            _httpContextAccessor?.HttpContext?.Session.SetString(key, JsonConvert.SerializeObject(value));
+            _httpContextAccessor?.HttpContext?.Session?.SetString(key, JsonConvert.SerializeObject(value));
         }
     }
 }
